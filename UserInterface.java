f8cf8c1ea0ce0180da0ee30782df7963f3cb3111
Sina-Ebadollahi <Ebadollahisina@Gmail.com>
2021-12-14 08:55:29 +0330

    third commit | project is finished

--- UserInterface.java
+++ UserInterface.java
@@ -1,9 +1,12 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
-public class UserInterface implements ActionListener {
+public class UserInterface implements ActionListener  {
+
     private ImageIcon programIcon;
     JFrame frame = new JFrame();
     // decimal textarea
@@ -21,7 +24,9 @@ public class UserInterface implements ActionListener {
     JLabel labelBinary = new JLabel("Binary");
     JLabel labelDecimal = new JLabel("Decimal");
     JLabel labelOctal = new JLabel("Octal");
-    JDialog n = new JDialog();
+    // contact label
+    JLabel contactLabel = new JLabel("Contact");
+    // url
 
 
 
@@ -45,38 +50,50 @@ public class UserInterface implements ActionListener {
         frame.add(textArea3);
         frame.add(textArea4);
         frame.add(resetBtn);
+        // label add
+        frame.getContentPane().add(labelBinary);
+        frame.getContentPane().add(labelDecimal);
+        frame.getContentPane().add(labelOctal);
+        frame.getContentPane().add(labelHex);
+        frame.getContentPane().add(contactLabel);
     }
     void setTextArea(){
-
+        // decimal textarea
         textArea.setVisible(true);
-        textArea.setBounds(100,200,100,100);
+        textArea.setBounds(100,50,200,100);
 
     }
 
     void setTextArea2() {
+        // binary textarea
         textArea2.setVisible(true);
-        textArea2.setBounds(100, 100, 100, 100);
+        textArea2.setBounds(100, 150, 200, 100);
+
 
     }
     void setTextArea3(){
+        // octal textarea
         textArea3.setVisible(true);
-        textArea3.setBounds(300,200,100,100);
+        textArea3.setBounds(100,250,200,100);
         textArea3.setBackground(new Color(224, 161, 161));
     }
     void setTextArea4(){
+        // hex textarea
         textArea4.setVisible(true);
-        textArea4.setBounds(400,200,100,100);
+        textArea4.setBounds(100,350,200,100);
 
     }
     void setBtn(){
         btn.setVisible(true);
-        btn.setBounds(300,300,100,100);
+        btn.setBounds(350,300,100,100);
         btn.setBackground(new Color(10,10,10));
         btn.setBorderPainted(false);
         btn.setCursor(new Cursor(Cursor.HAND_CURSOR));
         btn.addActionListener(this::actionPerformed);
+        btn.setForeground(new Color(255,255,255));
 
     }
+
     @Override
     public void actionPerformed(ActionEvent e) {
         try {
@@ -124,17 +141,39 @@ public class UserInterface implements ActionListener {
 
     void setResetBtn(){
         resetBtn.setVisible(true);
-        resetBtn.setBounds(100,300,100,100);
+        resetBtn.setBounds(350,150,100,100);
         resetBtn.setBackground(new Color(10,10,10));
         resetBtn.setBorderPainted(false);
         resetBtn.setCursor(new Cursor(Cursor.HAND_CURSOR));
         resetBtn.addActionListener(this::resetButtonClickAction);
+        resetBtn.setForeground(new Color(255,255,255));
 
     }
     public void resetButtonClickAction(ActionEvent e){
         textAreaReset();
     }
-
+    void setLabel(){
+        // decimal
+        labelDecimal.setVisible(true);
+        labelDecimal.setText("Decimal :");
+        labelDecimal.setBounds(0,50,100,100);
+        labelDecimal.setFont(new Font(Font.SANS_SERIF,Font.BOLD,20));
+        // binary
+        labelBinary.setVisible(true);
+        labelBinary.setText("Binary :");
+        labelBinary.setBounds(0,150,100,100);
+        labelBinary.setFont(new Font(Font.SANS_SERIF,Font.BOLD,20));
+        // octal
+        labelOctal.setVisible(true);
+        labelOctal.setText("Octal :");
+        labelOctal.setBounds(0,250,100,100);
+        labelOctal.setFont(new Font(Font.SANS_SERIF,Font.BOLD,20));
+        // octal
+        labelHex.setVisible(true);
+        labelHex.setText("Hex :");
+        labelHex.setBounds(0,350,100,100);
+        labelHex.setFont(new Font(Font.SANS_SERIF,Font.BOLD,20));
+    }
 
 
     void textAreaReset(){
@@ -142,6 +181,53 @@ public class UserInterface implements ActionListener {
         this.textArea2.setText("");
         this.textArea3.setText("");
         this.textArea4.setText("");
+    }
+    void setContactLabel(){
+        contactLabel.setVisible(true);
+        contactLabel.setFont(new Font(Font.SERIF, Font.ITALIC,15));
+        contactLabel.setBounds(400,0,50,50);
+        contactLabel.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        contactLabel.setForeground(new Color(60,60,60));
+        contactLabel.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e)  {
+                try{
+                    openBrowser();
+                }catch(URISyntaxException err){
+                    System.out.println(err.getMessage());
+                }
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
+    }
+    private void openBrowser() throws URISyntaxException {
+        if(Desktop.isDesktopSupported()){
+            try{
+                Desktop.getDesktop().browse(new URI("https://telegram.me/sina_eba"));
+            }catch(IOException e){
+                JOptionPane panel  = new JOptionPane();
+                panel.setToolTipText(e.getMessage());
+            }
+        }
     }
 //    void labelAdd(){
 //        labelHex.add(textArea);
@@ -161,5 +247,8 @@ public class UserInterface implements ActionListener {
         setTextArea2();
         setTextArea3();
         setTextArea4();
+        setLabel();
+        setContactLabel();
     }
 }
+
